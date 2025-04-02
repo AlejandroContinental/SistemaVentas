a7cdd05290e6be754e00b897859a48964fce6080
Rodrigo <roddrigoyosuea@gmail.com>
2025-04-01 20:15:07 -0500

    V02

---
+++ SistemaVenta/Datos/Conexion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class Conexion
+    {
+        //cadena conexion
+        static string cadena = "Server=localhost;Database=TallerMecanico;Integrade Security=True";
+        SqlConnection conexion = new SqlConnection(cadena);
+    }
+}
